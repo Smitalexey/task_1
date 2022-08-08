d07a2f6e01f5b0e98c9566e9fe3c06d1e971bb0f
Aleksey <smit_alexey@mail.ru>
2022-08-08 17:02:05 +0500

    Определение четности числа

---
+++ Task3/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine ("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0) 
+Console.WriteLine ("Число является четным");
+else 
+Console.WriteLine("Число является нечетным");
