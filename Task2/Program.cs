39f36cf6b9d3cfde8ef0dea7fb9671655aed1339
Aleksey <smit_alexey@mail.ru>
2022-08-08 16:26:58 +0500

    Определение максимального из трех чисел

---
+++ Task2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine ("Введите три целых числа");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+Console.WriteLine("Максимальное число - это");
+Console.WriteLine(max);
