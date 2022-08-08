fb669f462138e6ce1384bba4220fb1205697cdda
Aleksey <smit_alexey@mail.ru>
2022-08-08 15:36:43 +0500

    Определение максимального и минимального из двух чисел

---
+++ Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("Введите два целых числа");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if(a > b)
+{
+Console.WriteLine("Первое число больше, второе число меньше");
+}
+else if(a < b)
+{
+Console.WriteLine("Второе число больше, первое число меньше");
+}
